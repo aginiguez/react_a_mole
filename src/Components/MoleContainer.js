47b94df0436e913e24189f73d9041f55fbdcb2ed
Anthony Iniguez <anthony@Anthonys-MacBook-Pro.local>
2023-03-12 22:16:54 -0700

    final

---
+++ src/Components/MoleContainer.js
@@ -0,0 +1,29 @@
+import Mole from './Mole'
+import React, { useState } from "react" 
+import EmptySlot from './EmptySlot'
+function MoleContainer(props){
+
+    let [displayMole, setDisplayMole] = useState(false)
+    const bop = () => {
+        props.setScore(props.score + 1)
+        setDisplayMole(false)
+    }
+
+    return (
+        <div>
+
+            {
+                displayMole==true 
+                ?
+                <Mole handleClick={bop} setDisplayMole={setDisplayMole}/>
+                :
+                <EmptySlot setDisplayMole={setDisplayMole}/>
+            }
+        
+        </div>
+    )
+
+
+}
+
+export default MoleContainer
